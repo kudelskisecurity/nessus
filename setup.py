4a56e4906063dc1d28b85ab837712ae4231d07c5
tharvik <tharvik@users.noreply.github.com>
2016-03-08 19:36:18 +0100

    we indeed need requests to run

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "libnessus",
-    version = "0.1",
-    packages = find_packages(exclude=['test']),
+    name="libnessus",
+    version="0.1",
+    packages=find_packages(exclude=['test']),
+    install_requires=['requests'],
 )
