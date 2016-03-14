ff95eca744ac2009240f51db1a9c22dac93dc13e
tharvik <tharvik@users.noreply.github.com>
2016-03-14 16:53:58 +0100

    `libnessus` to `nessus`

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="libnessus",
+    name="nessus",
     version="0.1",
     packages=find_packages(exclude=['test']),
     install_requires=['requests'],
