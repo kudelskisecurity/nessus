f14fb3e5dc1f8b651e5da71c8b62feb7aa4edc5a
tharvik <tharvik@users.noreply.github.com>
2016-06-16 11:56:53 +0200

    add some Protocol

--- nessus/scans.py
+++ nessus/scans.py
@@ -3,6 +3,7 @@ sub modules for everything about the scans
 """
 
 from enum import Enum
+from pprint import pprint
 from uuid import uuid4
 
 from typing import Iterable, Mapping, Union, Optional, MutableMapping
@@ -771,6 +772,7 @@ class Transport(Enum):
 
 
 class Protocol(Enum):
+    ajp13 = 'ajp13'
     cifs = 'cifs'
     dns = 'dns'
     irc = 'irc'
@@ -828,6 +830,7 @@ class NessusScanPluginOutput(Object):
         plugin_output = str(json_dict['plugin_output'])
         hosts = str(json_dict['hosts'])
         severity = int(json_dict['severity'])
+        pprint(json_dict['ports'])
         ports = [NessusScanPluginOutputPort.from_json(k, v) for k, v in json_dict['ports'].items()]
 
         return NessusScanPluginOutput(plugin_output, hosts, severity, ports)
